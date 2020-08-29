2505089bde2246e2fc54ecc0850109afd185d840
Akeem Amusat <aoamusat@gmail.com>
2020-08-29 17:07:03 +0800

    Removed Products Link and update Github Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,7 +64,7 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Portfolio
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
@@ -72,14 +72,11 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/aoamusat"
                 target="_blank"
                 rel="noopener noreferrer"
               >
